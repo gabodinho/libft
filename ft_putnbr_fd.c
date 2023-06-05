3e890eedbdf8321fda3f01c640f1d043155ff6bd
Gabriel Giertzuch <ggiertzu@c4a7c8.42berlin.de>
2023-06-05 20:27:47 +0200

    libft_final

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: ggiertzu <ggiertzu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 02:58:31 by ggiertzu          #+#    #+#             */
-/*   Updated: 2023/05/25 02:57:49 by ggiertzu         ###   ########.fr       */
+/*   Updated: 2023/06/04 22:03:22 by ggiertzu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_recu(int n, int fd)
+static void	ft_recu(long n, int fd)
 {
 	if (n / 10 > 0)
 		ft_recu(n / 10, fd);
@@ -23,17 +23,15 @@ static void	ft_recu(int n, int fd)
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
 	{
 		write(fd, "-", 1);
-		n = n * -1;
+		nb = nb * -1;
 	}
-	ft_recu(n, fd);
+	ft_recu(nb, fd);
 	return ;
 }
 /*
